9f065f59ee7ff3618b344ad99ce95ea0f3284627
Irene-miao <71961907+Irene-miao@users.noreply.github.com>
2021-05-25 16:05:49 +0800

    Add files via upload

---
+++ part1/courseinfo2/Total.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Total = (props) => {
+    return (
+        <div>
+            <p>There are a total of {props.number} exercises.</p>
+        </div>
+    )
+}
+
+export default Total;
