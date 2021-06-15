2c4febbc537cf9abc9ebfd9dd414205a2a22bc72
Irene-miao <71961907+Irene-miao@users.noreply.github.com>
2021-06-15 21:00:18 +0800

    Add files via upload
    Added vote button for the displayed anecdote

--- part1/unicafe/App.js
+++ part1/unicafe/App.js
@@ -1,64 +1,46 @@
 import "./App.css";
 import React, { useState } from "react";
-import ButtonGrp from "./ButtonGrp";
-import Display from './Display';
-import StatisticList from "./StatisticList";
-
 
 function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients",
+  ];
 
-  const handleGoodClick = () => {
-    setGood(good + 1);
-  };
+  const [selected, setSelected] = useState("");
+  const [results, setResults] = useState({});
 
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1);
-  };
+  function getRandomItem() {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    const item = anecdotes[randomIndex];
+    setSelected(item);
+  }
 
-  const handleBadClick = () => {
-   setBad( bad + 1);
-  };
-
-  const all = good+bad+neutral;
-  
-  const average = (all/3).toFixed(2);
-
-
-    let percent;
-
-    if (good !== 0) {
-     percent = <p>&nbsp; {((good/all)*100).toFixed(2)} %</p>
-    } else {
-      percent = <p>&nbsp; 0 %</p>
+  const getVote = () => {
+    for (let i = 0; i < anecdotes.length; i++) {
+      const anecdote = anecdotes[i];
+      if (anecdote === selected) {
+        setResults({ ...anecdotes, [selected]: (results[selected] ?? 0) + 1 });
+      }
+      console.log(results);
     }
-  
+  };
 
   return (
-    <div className='App'>
-     <Display title='give feedback'/>
-     <div>
-     <ButtonGrp
-      onGoodClick={handleGoodClick}
-      good='good'
-      onNeutralClick={handleNeutralClick}
-      neutral='neutral'
-      onBadClick={handleBadClick}
-      bad='bad'
-      />
-     </div>
-      <Display title='statistics' />
-      <StatisticList 
-     bad={bad}
-     percent={percent}
-     good={good}
-     neutral={neutral}
-     average={average}
-     all={all}
-      />
+    <div className="App">
+      <div>{selected}</div>
+      <div>
+     <p>has {results[selected]} votes.</p>
+      </div>
+      <button onClick={getVote}>vote</button>
+      <button style={{ margin: "10px" }} onClick={getRandomItem}>
+        random
+      </button>
     </div>
   );
 }
