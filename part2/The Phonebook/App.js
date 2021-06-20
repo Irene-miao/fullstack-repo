03b24f18e0bd87076db8c3c6823bd04c711d99fc
Irene-miao <71961907+Irene-miao@users.noreply.github.com>
2021-06-20 21:53:17 +0800

    Add files via upload
    Implement a search field that can be used to filter the list of people by name

--- part2/The Phonebook/App.js
+++ part2/The Phonebook/App.js
@@ -4,10 +4,15 @@ import React, { useState } from "react";
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: 12345678 }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]); 
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber] = useState('');
+  const [ showAll, setShowAll] = useState(true);
+  const [ searchName, setSearchName] = useState('');
 
   const addName = (event) => {
     event.preventDefault();
@@ -31,15 +36,32 @@ const handleNameChange = (event) => {
   setNewName(event.target.value);
 };
 
+const handleSearchNameChange = (event) => {
+  console.log(event.target.value);
+  setShowAll(!showAll);
+  setSearchName(event.target.value);
+  
+};
+
 const handleNumberChange = (event) => {
   console.log(event.target.value);
   setNewNumber(event.target.value);
 };
 
+const personsToShow = showAll ? persons :
+persons.filter(person => person.name.toLowerCase().includes(searchName));
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
+      <div>
+          filter shown with 
+          <input 
+          value={searchName}
+         onChange={handleSearchNameChange} 
+         />
+        </div>
         <div>
           name: 
           <input 
@@ -59,7 +81,7 @@ const handleNumberChange = (event) => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => 
+      {personsToShow.map(person => 
         (<div key={person.name}>
         <p>
           {person.name} &nbsp; {person.number}
