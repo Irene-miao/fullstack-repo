441aaa2d27bf5ba55442857d990f1c629b5ee896
Irene-miao <71961907+Irene-miao@users.noreply.github.com>
2021-06-22 22:06:09 +0800

    Add files via upload
    data is fetched from the server using the axios-library and useEffect.

--- part2/The Phonebook/App.js
+++ part2/The Phonebook/App.js
@@ -1,20 +1,29 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
 import Persons from "./components/Persons";
+import axios from 'axios';
+
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [showAll, setShowAll] = useState(true);
   const [searchName, setSearchName] = useState("");
+
+  useEffect(() => {
+    console.log('effect');
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log(response.data);
+      setPersons(response.data);
+    })
+  }, []);
+
+  console.log('render', persons.length, 'persons');
 
   const addName = (event) => {
     event.preventDefault();
