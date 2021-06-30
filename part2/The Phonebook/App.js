a3a663a7357a1e2ef7fef9d02a9f5caf875cf75c
Irene-miao <71961907+Irene-miao@users.noreply.github.com>
2021-06-30 21:14:23 +0800

    Add files via upload
    if a number is added to an already existing user, the new number will replace the old number.

--- part2/The Phonebook/App.js
+++ part2/The Phonebook/App.js
@@ -3,7 +3,8 @@ import React, { useState, useEffect } from "react";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
 import Persons from "./components/Persons";
-import backend from './services/backend';
+import backend from "./services/backend";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -11,18 +12,17 @@ const App = () => {
   const [newNumber, setNewNumber] = useState("");
   const [showAll, setShowAll] = useState(true);
   const [searchName, setSearchName] = useState("");
+  const [newMessage, setNewMessage] = useState("");
 
   useEffect(() => {
-    console.log('effect');
-    backend
-    .getAll()
-    .then(response => {
+    console.log("effect");
+    backend.getAll().then((response) => {
       console.log(response.data);
       setPersons(response.data);
-    })
+    });
   }, []);
 
-  console.log('render', persons.length, 'persons');
+  console.log("render", persons.length, "persons");
 
   // Create new Person Object
   const addPerson = (event) => {
@@ -33,29 +33,40 @@ const App = () => {
     };
 
     backend
-    .create(personObject) // Object sent to server using axios post method
-    .then(response => {
-      setPersons(persons.concat(response.data)); //Save response sent from server to variable
-      setNewName("");
-      setNewNumber("");
-    });
+      .create(personObject) // Object sent to server using axios post method
+      .then((response) => {
+        setPersons(persons.concat(response.data)); //Save response sent from server to variable
+        setNewName("");
+        setNewNumber("");
+        setNewMessage(`Added ${response.data.name}`);
+        setTimeout(() => {
+          setNewMessage(null);
+        }, 5000);
+      });
 
-    persons.forEach((person) => {
-      if (newName === person.name) {
-        alert(`${newName} is already added to phonebook, replace the old number with a new one?`);
-        persons.pop();
-        const character = persons.find(p => p.id === person.id);
-        const changedPerson = {...character, number: newNumber};
-
-        backend
-        .update(person.id, changedPerson)
-        .then(response => {
-          console.log(response.data);
-          setPersons(persons.map(p => p.id !== person.id ? p : response.data))
-        });
-      };
+      persons.forEach((person) => {
+        if (newName === person.name) {
+          alert(
+            `${newName} is already added to phonebook, replace the old number with a new one?`
+          );
+          persons.pop();
+          const character = persons.find((p) => p.id === person.id); //Make a copy of the person object to modify
+          const changedPerson = { ...character, number: newNumber }; //New person object with amended property 
+  
+          // New person object sent with PUT request to backend server to replace old note
+          backend.update(person.id, changedPerson).then((response) => {
+            console.log(response.data);
+            setPersons(persons.map(p => p.id !== person.id ? p : response.data)); // Set persons state to new array contain old persons array + updated person
+            persons.pop();
+            setNewMessage(`Changed ${person.name} 's phone number`);
+            setTimeout(() => {
+              setNewMessage(null);
+            }, 5000);
+          });
+        }
     });
-  };
+    };
+  
 
   const handleNameChange = (event) => {
     console.log(event.target.value);
@@ -73,17 +84,15 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
-  const handleDelete =(id) => {
+  const handleDelete = (id) => {
     console.log(`${id} to be deleted`);
-    backend
-    .remove(id)
-    .then(response => {
+    backend.remove(id).then((response) => {
       const result = window.confirm("Do you want to delete?");
       if (result) {
-        window.open("exit.html", "Deleted successfully!")
-      };
+        window.open("exit.html", "Deleted successfully!");
+      }
       console.log(`Delete success ${response.data}`);
-      setPersons(persons.filter(person => person.id !== id));
+      setPersons(persons.filter((person) => person.id !== id));
     });
   };
 
@@ -96,7 +105,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-
+      <Notification message={newMessage} />
       <Filter value={searchName} onChange={handleSearchNameChange} />
 
       <h3>Add a new person details</h3>
@@ -110,15 +119,17 @@ const App = () => {
       />
 
       <h2>Numbers</h2>
-<ul>
-{personsToShow.map((person) => 
-  <Persons 
-  key = {person.id}
-      person={person}
-      handleDelete={() => handleDelete(person.id)}
-      />
-)}
-</ul>
+      <div>
+      <ul>
+        {personsToShow.map((person) => (
+          <Persons
+            key={person.id}
+            person={person}
+            handleDelete={() => handleDelete(person.id)}
+          />
+        ))}
+      </ul>
+      </div>
     </div>
   );
 };
