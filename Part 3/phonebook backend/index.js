385b52f0672c399dde99f46294fd5f4ec6a6e169
Irene-miao <71961907+Irene-miao@users.noreply.github.com>
2021-07-06 18:17:46 +0800

    Add files via upload
    new phonebook entries can be added by making HTTP POST requests, generate a new id for the phonebook entry

--- Part 3/phonebook backend/index.js
+++ Part 3/phonebook backend/index.js
@@ -1,7 +1,7 @@
 const { response } = require('express');
 const express = require('express');
 const app = express();
-
+app.use(express.json());
 
 let persons = [
     { 
@@ -54,7 +54,40 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(p => p.id !== id);
 
   response.status(204).end();
-})
+});
+
+// Post a new person
+app.post('/api/persons', (request, response) => {
+  
+  const generateId = (max) => {
+const id = persons.length > 0 ? Math.floor(Math.random() * max) : 0;
+return id 
+  };
+
+  const person = request.body;
+
+  if (!person.name) {
+    return response.status(400).json({
+      error:'name missing'
+    })
+  };
+
+  if (!person.number) {
+    return response.status(400).json({
+    error: 'number missing'
+  })
+  };
+
+  const personDetail = {
+    name: person.name,
+    number: person.number,
+    date: new Date(),
+    id: generateId(100),
+  };
+
+  persons = persons.concat(personDetail);
+  response.json(personDetail);
+});
 
 const PORT = 3001;
 app.listen(PORT);
