96bc4226a90f2df9dd4e8293655a08b3541f0e12
Irene-miao <71961907+Irene-miao@users.noreply.github.com>
2021-08-12 22:04:00 +0800

    Add files via upload
    Modify adding new blogs so that when a new blog is created, any user from the database is designated as its creator 
    Modify listing all blogs so that the creator's user information is displayed with the blog and listing all users also displays the blogs created by each user

--- Part 4/controllers/blogs.js
+++ Part 4/controllers/blogs.js
@@ -2,34 +2,50 @@
 
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
+// Get all blogs
 blogsRouter.get('/', async (request, response) => {
-   const blogs = await Blog.find({})
+   const blogs = await Blog.find({}).populate('user', {username: 1, name: 1, id: 1 })
   response.json(blogs.map(blog => blog.toJSON()))  
 })
 
+
+// Create new blog
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
 /*if (!body.title || !body.author || !body.url) {
   response.status(400).json({ error: 'field missing' })
 }*/
+
+const users = await User.find({})
+const user = users[1]
+console.log(user._id)
     
 const blog = new Blog({
   title: body.title,
   author: body.author,
   url: body.url,
   likes: body.likes,
+  user: user._id
 })
 
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
    response.json(savedBlog)
 })
 
+
+// Delete a blog
 blogsRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndRemove(request.params.id)
   response.status(204).end()
 })
 
+
+// Update a blog
 blogsRouter.put('/:id', async (request, response) => {
   const body = request.body
 
--- Part 4/controllers/users.js
+++ Part 4/controllers/users.js
@@ -28,7 +28,7 @@ if (!body.password) {
 
 // Get all users
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User.find({}).populate('blogs', { title: 1, url: 1, likes: 1 })
     response.json(users)
 })
 
