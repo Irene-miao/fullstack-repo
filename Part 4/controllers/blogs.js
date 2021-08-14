c5bc8505c8ff24624f0fd5b21f3e9140edf25794
Irene-miao <71961907+Irene-miao@users.noreply.github.com>
2021-08-14 21:14:17 +0800

    Add files via upload
    Change the delete blog operation so that a blog can be deleted only by the user who added the blog

--- Part 4/controllers/blogs.js
+++ Part 4/controllers/blogs.js
@@ -20,7 +20,7 @@ blogsRouter.post('/', async (request, response) => {
 /*if (!body.title || !body.author || !body.url) {
   response.status(400).json({ error: 'field missing' })
 }*/
-console.log(request.token)
+
 const decodedToken = jwt.verify(request.token, process.env.SECRET)
 if (!request.token || !decodedToken) {
   return response.status(401).json({ error: 'token missing or invalid' })
@@ -47,8 +47,23 @@ const blog = new Blog({
 
 // Delete a blog
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
+  const blog = await Blog.findById(request.params.id)
+  
+ 
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  
+  if (!request.token || !decodedToken) {
+    return response.status(401).json({ error: 'Unable to delete blog due to missing token' })
+  }
+  const user = await User.findById(decodedToken.id)
+  
+  if ( blog.user.toString() === user.id.toString()) {
+    await Blog.findByIdAndRemove(blog._id)
+    response.status(204).end()
+  } else {
+    return response.status(401).json({ error: 'Unable to delete blog due to wrong user'})
+  }
+  
 })
 
 
