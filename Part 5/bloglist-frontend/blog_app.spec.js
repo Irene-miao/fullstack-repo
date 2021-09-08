5f87381869291c1dd8aa771dd7aad54b70113cab
Irene-miao <71961907+Irene-miao@users.noreply.github.com>
2021-09-08 21:04:29 +0800

    Add files via upload
    Make tests for logging in. Test both successful and unsuccessful log in attempts. Make a new user in the beforeEach block for the tests.

--- Part 5/bloglist-frontend/blog_app.spec.js
+++ Part 5/bloglist-frontend/blog_app.spec.js
@@ -6,7 +6,13 @@
 // https://on.cypress.io/writing-first-test
 describe('Blog app', function() {
     beforeEach(function() {
-        //cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        const user = {
+            name: 'Test One',
+            username: 'testone',
+            password: 'testing'
+        }
+        cy.request('POST', 'http://localhost:3003/api/users/', user)
         cy.visit('http://localhost:3000')
     })
 
@@ -15,5 +21,25 @@ describe('Blog app', function() {
         cy.contains('username')
         cy.contains('password')
         cy.contains('Submit')
+    })
+
+    describe('Login', function() {
+        it('succeeds with correct credentials', function() {
+            cy.get('#username').type('testone')
+            cy.get('#password').type('testing')
+            cy.contains('Submit').click()
+            cy.contains('logout').click()
+        })
+
+        it('fails with wrong credentials', function() {
+            cy.get('#username').type('testone')
+            cy.get('#password').type('wrong')
+            cy.contains('Submit').click()
+
+            cy.get('.error')
+            .should('contain', 'invalid username or password')
+            .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+        })
     })
 })
