86957b944ea3976a41a59975f81e5498d914bda1
Irene-miao <71961907+Irene-miao@users.noreply.github.com>
2021-11-28 21:46:36 +0800

    Add files via upload
    Define action creator functions in the file src/state/reducer.tsxand refactor the code to use them

--- part 9/Patientor-frontend/src/App.tsx
+++ part 9/Patientor-frontend/src/App.tsx
@@ -4,11 +4,12 @@ import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import { Button, Divider, Header, Container } from "semantic-ui-react";
 
 import { apiBaseUrl } from "./constants";
-import { useStateValue } from "./state";
-import { Patient } from "./types";
+import { useStateValue, setPatientList } from "./state";
 
 import PatientListPage from "./PatientListPage";
 import PatientDetails from "./components/PatientDetails";
+import { Patient } from "./types";
+
 
 const App = () => {
   const [, dispatch] = useStateValue();
@@ -20,7 +21,7 @@ const App = () => {
         const { data: patientListFromApi } = await axios.get<Patient[]>(
           `${apiBaseUrl}/api/patients`
         );
-        dispatch({ type: "SET_PATIENT_LIST", payload: patientListFromApi });
+        dispatch(setPatientList(patientListFromApi));
       } catch (e) {
         console.error(e);
       }
@@ -50,3 +51,7 @@ const App = () => {
 };
 
 export default App;
+/*function patientListFromApi(patientListFromApi: any): import("./state").Action {
+  throw new Error("Function not implemented.");
+}*/
+
